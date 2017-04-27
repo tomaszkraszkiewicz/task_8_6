4b9a27a3eedd06b6f297fada6d90c6552e4e3039
tomaszkraszkiewicz <tomasz.kraszkiewicz@gmail.com>
2017-04-27 22:24:09 +0200

    quotation changed and end of line added

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,11 @@
 var a = prompt('podaj liczbę');
 var b = prompt('podaj liczbę');
 var value = (a * a) + (2 * a * b) - (b * b);
-alert(value)
-
-
+alert(value);
 if (value > 0 ) {
-  console.log(‘Liczba jest dodatnia’);
+  console.log("Liczba jest dodatnia");
 } else if (value < 0 ) {
-  console.log(‘Liczba jest ujemna’);
+  console.log("Liczba jest ujemna");
 } else {
-  console.log(‘Liczba równa zero’);
+  console.log("Liczba równa zero");
 }
-
-
-
